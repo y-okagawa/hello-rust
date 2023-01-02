955d5459765194e9f45aee0ebcdd4247d5b72c12
y-okagawa <yuya@okagawa.dev>
2023-01-03 00:56:13 +0900

    chore: use serde

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
 use actix_web::{
-    http::header::ContentType, App, HttpResponse,
+    web,
+    http::header::ContentType,
+    App,
+    HttpResponse,
     HttpServer,
 };
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct UserQuery {
+    name: String,
+    age: u32,
+}
+
+#[derive(Serialize)]
+struct UserResponse {
+    user: String,
+}
+
+#[actix_web::get("/users")]
+async fn users(
+    query: web::Query<UserQuery>,
+) -> HttpResponse {
+    let query = query.into_inner();
+    let message = format!(
+        "name: {}. age: {}",
+        query.name, query.age
+    );
+    let body = UserResponse { user: message };
+    HttpResponse::Ok().json(body)
+}
 
 #[actix_web::get("/")]
 async fn hello() -> HttpResponse {
@@ -12,7 +40,10 @@ async fn hello() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new()
+        .service(hello)
+        .service(users)
+    )
         .bind(("0.0.0.0", 8080))?
         .run()
         .await
